c1d4c8ff6c153c39aa9df5bde1303bd4f37ee59c
Infixo <swidwins@gmail.com>
2025-09-29 17:39:37 +0200

    Event logger and better vehicle choosing

--- Patches/PlanEvaluateRoute_Patches.cs
+++ Patches/PlanEvaluateRoute_Patches.cs
@@ -1,15 +1,17 @@
 ﻿using HarmonyLib;
-using Utilities;
 using STM.Data.Entities;
 using STM.GameWorld;
 using STM.GameWorld.AI;
 using STM.GameWorld.Commands;
 using STM.GameWorld.Users;
+using STM.UI.Floating;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Utilities;
 
 namespace AITweaks.Patches;
 
@@ -17,9 +19,11 @@ namespace AITweaks.Patches;
 [HarmonyPatch(typeof(PlanEvaluateRoute))]
 public static class PlanEvaluateRoute_Patches
 {
+
     [HarmonyPatch("EvaluateLine"), HarmonyPrefix]
     public static bool PlanEvaluateRoute_EvaluateLine_Prefix(PlanEvaluateRoute __instance, Company company, VehicleBaseUser[] vehicles, GameScene scene, HubManager manager)
     {
+
         VehicleEvaluation _evaluation = default(VehicleEvaluation);
         for (int i = 0; i < vehicles.Length; i++)
         {
@@ -61,6 +65,7 @@ public static class PlanEvaluateRoute_Patches
                                     }
                                     decimal _weight4 = __instance.CallPrivateMethod<long>("GetBalance", [_best]) * 2;
                                     manager.AddNewPlan(new GeneratedPlan(_weight4 / (decimal)_price6, _settings, _price6, _best));
+                                    LogEvent("UPPLAN1", _best, _upgrade);
                                 }
                                 else if (company.AI != null)
                                 {
@@ -72,6 +77,7 @@ public static class PlanEvaluateRoute_Patches
                                         _price5 = 1L;
                                     }
                                     company.AI.AddNewPlan(new GeneratedPlan(_weight3 / (decimal)_price5, _settings, _price5, _best));
+                                    LogEvent("UPPLAN2", _best, _upgrade);
                                 }
                                 else
                                 {
@@ -81,6 +87,7 @@ public static class PlanEvaluateRoute_Patches
                                         scene.Session.Commands.Add(new CommandSell(company.ID, _best.ID, _best.Type));
                                         scene.Session.Commands.Add(new CommandNewRoute(company.ID, _settings, open: false));
                                     });
+                                    LogEvent("UPGRADE1", _best, _upgrade);
                                 }
                                 return false;
                             }
@@ -92,6 +99,7 @@ public static class PlanEvaluateRoute_Patches
                                 if (_best_e > (decimal)_upgrade.Real_min_passengers)
                                 {
                                     scene.Session.Commands.Add(new CommandSell(company.ID, _worst2.ID, _worst2.Type, manager));
+                                    LogEvent("UPSELL1", _worst2);
                                     if (manager != null)
                                     {
                                         long _price4 = _upgrade.GetPrice(scene, company, scene.Cities[_best.Hub.City].User);
@@ -102,6 +110,7 @@ public static class PlanEvaluateRoute_Patches
                                             _price4 = 1L;
                                         }
                                         manager.AddNewPlan(new GeneratedPlan(_weight2 / (decimal)_price4, _settings, _price4, _best));
+                                        LogEvent("UPPLAN3", _best, _upgrade);
                                     }
                                     else if (company.AI != null)
                                     {
@@ -113,6 +122,7 @@ public static class PlanEvaluateRoute_Patches
                                             _price3 = 1L;
                                         }
                                         company.AI.AddNewPlan(new GeneratedPlan(_weight / (decimal)_price3, _settings, _price3, _best));
+                                        LogEvent("UPPLAN4", _best, _upgrade);
                                     }
                                     else
                                     {
@@ -122,6 +132,7 @@ public static class PlanEvaluateRoute_Patches
                                             scene.Session.Commands.Add(new CommandSell(company.ID, _best.ID, _best.Type));
                                             scene.Session.Commands.Add(new CommandNewRoute(company.ID, _settings, open: false));
                                         });
+                                        LogEvent("UPGRADE2", _best, _upgrade);
                                     }
                                     return false;
                                 }
@@ -145,6 +156,7 @@ public static class PlanEvaluateRoute_Patches
                             _price2 += _best.Hub.GetNextLevelPrice(scene.Session);
                         }
                         manager.AddNewPlan(new GeneratedPlan(1m, new NewRouteSettings(_best), _price2));
+                        LogEvent("UPPLAN5", _best);
                         return false;
                     }
                     if (company.AI != null)
@@ -155,6 +167,7 @@ public static class PlanEvaluateRoute_Patches
                             _price += _best.Hub.GetNextLevelPrice(scene.Session);
                         }
                         company.AI.AddNewPlan(new GeneratedPlan(1m, new NewRouteSettings(_best), _price));
+                        LogEvent("UPPLAN6", _best);
                         return false;
                     }
                     if (_best.Hub.Full())
@@ -164,8 +177,10 @@ public static class PlanEvaluateRoute_Patches
                             return false;
                         }
                         scene.Session.Commands.Add(new CommandUpgradeHub(company.ID, _best.Hub.City));
+                        LogEvent("HUBFULL", _best);
                     }
                     scene.Session.Commands.Add(new CommandNewRoute(company.ID, new NewRouteSettings(_best), open: false));
+                    LogEvent("UPGRADE3", _best);
                 }
                 else if (vehicles.Length > 1)
                 {
@@ -174,6 +189,7 @@ public static class PlanEvaluateRoute_Patches
                     {
                         // If there are 2+ vehicles and the line is average (no upgrade, no downgrade) BUT generates loses, it sells 1 vehicle
                         scene.Session.Commands.Add(new CommandSell(company.ID, _worst.ID, _worst.Type, manager));
+                        LogEvent("UPSELL2", _worst);
                     }
                 }
                 else if (__instance.CallPrivateMethod<bool>("AllVehiclesNotDelivering", [vehicles]))
@@ -206,6 +222,7 @@ public static class PlanEvaluateRoute_Patches
                     newRouteSettings2.upgrade = new UpgradeSettings(_worst4, scene);
                     scene.Session.Commands.Add(new CommandSell(company.ID, _worst4.ID, _worst4.Type, manager));
                     scene.Session.Commands.Add(new CommandNewRoute(company.ID, newRouteSettings2, open: false, manager));
+                    LogEvent("DOWNGRADE1", _worst4, _downgrade2);
                 });
             }
             else
@@ -213,6 +230,7 @@ public static class PlanEvaluateRoute_Patches
                 // TODO: this is dangerous; it wants to downgrade the line but there is no "worse" vehicle e.g. already Tier1, so it will sell it!
                 // not sure; the while only starts if there is more than 1 vehicle, so this would sell 2nd to last
                 scene.Session.Commands.Add(new CommandSell(company.ID, _worst4.ID, _worst4.Type, manager));
+                LogEvent("DOWNSELL1", _worst4);
             }
             VehicleEvaluation _e = new VehicleEvaluation(_worst4);
             _evaluation.Remove(_e);
@@ -236,20 +254,114 @@ public static class PlanEvaluateRoute_Patches
                 newRouteSettings.upgrade = new UpgradeSettings(_worst3, scene);
                 scene.Session.Commands.Add(new CommandSell(company.ID, _worst3.ID, _worst3.Type, manager));
                 scene.Session.Commands.Add(new CommandNewRoute(company.ID, newRouteSettings, open: false, manager));
+                LogEvent("DOWNGRADE2", _worst3, _downgrade);
             });
         }
         else if (_worst3.Entity_base.Tier == 1 && _worst3.Age >= 6 && _worst3.Balance.GetRollingYear() < 10000000 && manager == null)
         {
+            // 2025-09-29 I don't want the last vehicle to be sold
+            if (company == scene.Session.GetPlayer() && vehicles.Length == 1)
+                return false;
             // DANGEROUS - can sell last Tier1 especially for new lines! it has 6 months to get to 10mil!!!
             scene.Session.Commands.Add(new CommandSell(company.ID, _worst3.ID, _worst3.Type, manager));
+            LogEvent("DOWNSELL2", _worst3);
         }
         else if (_worst3.Entity_base.Tier == 1 && _worst3.Age >= 12 && _worst3.Balance.GetRollingYear() < 5000000 && manager == null)
         {
+            // 2025-09-29 I don't want the last vehicle to be sold
+            if (company == scene.Session.GetPlayer() && vehicles.Length == 1)
+                return false;
             // DANGEROUS - can sell last Tier1 especially for new lines! it has 12 months to get to 5mil!!!
             scene.Session.Commands.Add(new CommandSell(company.ID, _worst3.ID, _worst3.Type, manager));
+            LogEvent("DOWNSELL3", _worst3);
+        }
+        return false;
+
+        // debug & tracking
+        //[MethodImpl(MethodImplOptions.NoInlining)]
+        void LogEvent(string type, VehicleBaseUser vh1, VehicleBaseEntity? vh2 = null)
+        {
+            string route = $"{vh1.Route.Last.Name} -> {vh1.Route.Current.Name} -> {vh1.Route.Destination.Name}";
+            string vh2txt = vh2 != null ? $"  vh2= {vh2.Tier} {vh2.Translated_name}" : "";
+            Log.Write($"""
+
+                city={scene.Cities[manager.Hub.City].User.Name} dec={type} veh={vehicles.Length} {route} wait={vh1.Route.GetWaiting()}
+                ..up={_evaluation.Upgrade} dn={_evaluation.Downgrade} num={_evaluation.samples} use={100*(int)_evaluation.throughput_now/(int)_evaluation.throughput_max} thr={_evaluation.throughput_min}/{_evaluation.throughput_now}/{_evaluation.throughput_max} bal={_evaluation.balance}
+                ..vh1= {vh1.Entity_base.Tier} {vh1.Entity_base.Translated_name}{vh2txt}
+            """);
+        }
+    }
+
+    /*
+    [HarmonyPatch("GetBetter"), HarmonyPrefix]
+    public static bool GetBetter_Prefix(PlanEvaluateRoute __instance, ref VehicleBaseEntity __result, Company company, VehicleCompanyEntity vehicle_company, VehicleBaseEntity entity, Hub hub, int range)
+    {
+        for (int i = 0; i < vehicle_company.Vehicles.Count; i++)
+        {
+            bool temp = __instance.CallPrivateMethod<bool>("IsBetter", [company, entity, vehicle_company.Vehicles[i], hub, range]);
+            if (vehicle_company.Vehicles[i].Type_name == entity.Type_name && temp)
+            {
+                __result = vehicle_company.Vehicles[i];
+                return false;
+            }
+        }
+        __result = null;
+        return false;
+    }
+    */
+
+    [HarmonyPatch("IsBetter"), HarmonyPrefix]
+    public static bool PlanEvaluateRoute_IsBetter_Prefix(PlanEvaluateRoute __instance, ref bool __result, Company company, VehicleBaseEntity original, VehicleBaseEntity next, Hub hub, int range)
+    {
+        __result = false;
+        if (next.Graphics.Entity == null || !next.CanBuy(company, hub.Longitude))
+        {
+            return false;
+        }
+        if (next is PlaneEntity _plane && _plane.Range < range)
+        {
+            return false;
+        }
+        if (original is TrainEntity _o && next is TrainEntity _n)
+        {
+            if (_n.Tier > _o.Tier || (_n.Tier == _o.Tier && _n.Max_capacity > _o.Max_capacity))
+            {
+                __result = __instance.GetPrivateField<long>("wealth") > next.Price;
+                return false;
+            }
+            return false;
+        }
+        if (next.Tier > original.Tier || (next.Tier == original.Tier && next.Capacity > original.Capacity))
+        {
+            __result = __instance.GetPrivateField<long>("wealth") > next.Price;
+            return false;
         }
         return false;
     }
+
+
+    [HarmonyPatch("IsWorse"), HarmonyPrefix]
+    public static bool IsWorse(PlanEvaluateRoute __instance, ref bool __result, Company company, VehicleBaseEntity original, VehicleBaseEntity next, Hub hub, int range)
+    {
+        if (next.Graphics.Entity == null)
+        {
+            return false;
+        }
+        if (next is PlaneEntity _plane && _plane.Range < range)
+        {
+            return false;
+        }
+        if (next.CanBuy(company, hub.Longitude) && original.Price > next.Price)
+        {
+            //if (original.Tier >= next.Tier)
+            //{
+                //return original.Real_min_passengers > next.Real_min_passengers;
+            //}
+            return true;
+        }
+        return false;
+    }
+
 
     /*
     [HarmonyPatch(typeof(VehicleEvaluation), "Evaluate"), HarmonyPrefix]
