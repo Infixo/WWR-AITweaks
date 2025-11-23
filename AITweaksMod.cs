78a793e7bb2357e25effe6cf41b4e331d387470e
Infixo <swidwins@gmail.com>
2025-11-23 22:37:15 +0100

    Adds source methods analyzer for easier patch maintenance.

--- AITweaksMod.cs
+++ AITweaksMod.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.InteropServices;
-using HarmonyLib;
 using Utilities;
 
 namespace AITweaks;
@@ -7,42 +6,17 @@ namespace AITweaks;
 
 public static class ModEntry
 {
-    public static readonly string harmonyId = "Infixo." + nameof(AITweaks);
-
-    // mod's instance and asset
-    //public static Mod instance { get; private set; }
-    //public static ExecutableAsset modAsset { get; private set; }
-    // logging
-    //public static ILog log = LogManager.GetLogger($"{nameof(InfoLoom)}").SetShowsErrorsInUI(false);
+    public static readonly string ModName = nameof(AITweaks);
 
     [UnmanagedCallersOnly]
     //[UnmanagedCallersOnly(EntryPoint = "InitializeMod")] // not needed when called via CLR
     //[ModuleInitializer] // only works with CLR, not native loads?
     public static int InitializeMod()
     {
-        DebugConsole.Show();
-        Log.Write($"WWR mod {nameof(AITweaks)} successfully started, harmonyId is {harmonyId}.");
-        try
+        if (ModInit.InitializeMod(ModName))
         {
-            // Harmony
-            var harmony = new Harmony(harmonyId);
-            //harmony.PatchAll(typeof(Mod).Assembly);
-            harmony.PatchAll();
-            var patchedMethods = harmony.GetPatchedMethods().ToArray();
-            Log.Write($"Plugin {harmonyId} made patches! Patched methods: " + patchedMethods.Length);
-            foreach (var patchedMethod in patchedMethods)
-            {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                Log.Write($"Patched method: {patchedMethod.Module.Name}:{patchedMethod.DeclaringType.Name}.{patchedMethod.Name}");
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            }
+            // do other stuff here to initialize
         }
-        catch (Exception ex)
-        {
-            Log.Write("EXCEPTION");
-            Log.Write(ex.ToString());
-        }
-        // do other stuff here to initialize
         return 0;
     }
 }
