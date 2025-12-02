8fe9e71ce7370f3e455c16e0bbc118ec12eb8ff9
Infixo <swidwins@gmail.com>
2025-12-02 20:17:03 +0100

    Fix for pathing problems.

---
+++ Patches/PathSearchFix.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using STM.GameWorld;
+
+namespace AITweaks.Patches;
+
+
+[HarmonyPatch]
+public static class PathSearchFix
+{
+    [HarmonyPatch(typeof(PathSearchNode), "GetNext"), HarmonyPrefix]
+    public static bool GetNext(PathSearchNode __instance, ref PathSearchNode __result, PathSearchData data, City destination, int overcrowd_factor, GameScene scene)
+    {
+        int _adjusted_start_cost = PathSearchNode.GetDistanceCost(data.current.City, data.add.City);
+        int _start_cost = __instance.start_cost + _adjusted_start_cost;
+        //_adjusted_start_cost *= 1 + (int)data.add.Accepts_indirect / 20 * overcrowd_factor;
+        //if (data.add.Routes.Count > 9)
+        //{
+            //_adjusted_start_cost /= Math.Max(data.add.Routes.Count / 5 - __instance.Step, 1);
+        //}
+        _adjusted_start_cost += __instance.Adjusted_start_cost;
+        __result = data.GetNode(data.add.City, _start_cost, _adjusted_start_cost, PathSearchNode.GetDistanceCost(data.add.City, destination), (ushort)(__instance.Step + 1), __instance.ID);
+        return false;
+    }
+}
